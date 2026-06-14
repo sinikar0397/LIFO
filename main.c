63c702175083706e80170aca8bc8a1e7b2d90cfd
Kim JuHwan <sinikar0397@gmail.com>
2026-06-14 22:07:28 +0900

    code refactoring(main_ui, main_bfs erased)

--- main.c
+++ main.c
@@ -1,160 +1,80 @@
+#include "./src/bfs/bfs.h"
+#include "./src/gui/display.h"
+#include "./src/gui/ui.h"
 #include "./src/headers.h"
-#include "./src/dfs/dfs.h"
 #include "./src/people/login.h"
 #include "./src/people/people.h"
 
-int main(void) {
-
-	printf("========================================\n");
-	printf(" Login System Test\n");
-	printf("========================================\n");
-
+int main(int argc, char **argv) {
+	SDL_Ui ui_asdf;
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
+	ui_asdf.window = window;
+	ui_asdf.renderer = renderer;
+	SDL_Ui *ui = &ui_asdf;
+	People *p = NULL;
+	People *people[MAX_PEOPLE] = {0};
+	MatchingInfo *infos =
+		(MatchingInfo *)calloc(MAX_PEOPLE, sizeof(MatchingInfo));
+		
 	login_init();
+	gui_initUi(ui);
+	int n = 0;
 
-	while (1) {
+	if (infos == NULL) {
+		return 1;
+	}
 
-		int cmd;
-
-		printf("\n");
-		printf("1. 회원가입\n");
-		printf("2. 로그인\n");
-		printf("3. ID 존재 여부 확인\n");
-		printf("4. 계정 정보 조회\n");
-		printf("5. Date Fit Survey 실행\n");
-		printf("6. 이상형-상대 성격 유사도 계산\n");
-		printf("7. 종료\n");
-		printf("입력 : ");
-
-		scanf("%d", &cmd);
-
-		switch (cmd) {
-
-		case 1: {
-			printf("\n[회원가입]\n");
-
-			People *p = login_sign_in_account();
-
-			printf("\n저장된 계정 정보\n");
-			people_print_people(p);
-
-			people_delete_people(p);
-
-			break;
-		}
-
-		case 2: {
-			printf("\n[로그인]\n");
-
-			People *p = login_log_in_account();
-
+	while (!ui->quit) {
+		switch (ui->next_state) {
+		case LOGIN:
 			if (p != NULL) {
-				printf("\n로그인 성공\n");
 				people_delete_people(p);
+				p = NULL;
 			}
-
+			p = display_showLogin(ui);
+			n = bfs_loadPeopleFromDatabase(people);
+			initMatchingInfos(infos, people, n);
+			if (p != NULL) {
+				char partner_id[MAX_ID_LEN] = "";
+				int has_partner =
+					match_store_find_accepted_partner(p->id, partner_id,
+													  sizeof(partner_id));
+				if (!has_partner && p->status == MATCHED &&
+					p->lover[0] != '\0') {
+					strncpy(partner_id, p->lover, sizeof(partner_id) - 1);
+					partner_id[sizeof(partner_id) - 1] = '\0';
+					has_partner = 1;
+				}
+				if (has_partner) {
+					strncpy(g_mst_partner_id, partner_id,
+							sizeof(g_mst_partner_id) - 1);
+					g_mst_partner_id[sizeof(g_mst_partner_id) - 1] = '\0';
+					ui->next_state = MST;
+				}
+			}
 			break;
-		}
-
-		case 3: {
-			char id[MAX_ID_LEN];
-
-			printf("검색할 ID : ");
-			scanf("%s", id);
-
-			if (login_does_ID_exist(id))
-				printf("존재하는 ID입니다.\n");
-			else
-				printf("존재하지 않는 ID입니다.\n");
-
+		case HOME:
+			display_showHome(ui, p);
 			break;
-		}
-
-		case 4: {
-			char id[MAX_ID_LEN];
-
-			printf("조회할 ID : ");
-			scanf("%s", id);
-
-			if (!login_does_ID_exist(id)) {
-				printf("존재하지 않는 ID입니다.\n");
-				break;
-			}
-
-			People *p = login_get_account(id);
-
-			if (p == NULL) {
-				printf("계정 로드 실패\n");
-				break;
-			}
-
-			people_print_people(p);
-
-			people_delete_people(p);
-
+		case DFS:
+			display_showDFS(ui, p);
 			break;
-		}
-
-		// case 5: {
-		// 	char type[MAX_TYPE_LEN];
-
-		// 	dfs_run_date_fit_survey(type, "Date Fit Survey");
-		// 	printf("\n저장 가능한 최종 유형 코드 : %s\n", type);
-
-		// 	break;
-		// }
-
-		case 6: {
-			char ideal_owner_id[MAX_ID_LEN];
-			char other_id[MAX_ID_LEN];
-
-			printf("이상형 기준이 되는 사용자 ID : ");
-			scanf("%19s", ideal_owner_id);
-			printf("상대 사용자 ID : ");
-			scanf("%19s", other_id);
-
-			if (!login_does_ID_exist(ideal_owner_id) || !login_does_ID_exist(other_id)) {
-				printf("존재하지 않는 ID가 있습니다.\n");
-				break;
-			}
-
-			People *ideal_owner = login_get_account(ideal_owner_id);
-			People *other = login_get_account(other_id);
-
-			if (ideal_owner == NULL || other == NULL) {
-				printf("계정 로드 실패\n");
-				people_delete_people(ideal_owner);
-				people_delete_people(other);
-				break;
-			}
-
-			int distance = dfs_type_distance(ideal_owner->love_type, other->type);
-			int similarity = dfs_type_similarity(ideal_owner->love_type, other->type);
-			int total_score = compat(ideal_owner, other);
-
-			printf("\n[%s의 이상형] %s\n", ideal_owner->id, ideal_owner->love_type);
-			dfs_print_type_description(ideal_owner->love_type);
-			printf("\n[%s의 성격] %s\n", other->id, other->type);
-			dfs_print_type_description(other->type);
-			printf("\nDistance = Depth(u) + Depth(v) - 2 * Depth(LCA(u, v)) = %d\n",
-				   distance);
-			printf("이상형-상대 성격 유사도 : %d / 100\n", similarity);
-			printf("양방향 예상 호감도 : %d / 200\n", total_score);
-
-			people_delete_people(ideal_owner);
-			people_delete_people(other);
-
+		case BFS:
+			display_showBFS(ui, p, infos, n, people);
 			break;
-		}
-
-		case 7: {
-			printf("프로그램 종료\n");
-			return 0;
-		}
-
-		default:
-			printf("잘못된 입력입니다.\n");
+		case MST:
+			display_showMST(ui, p);
+			break;
+		case PROFILE:
+			display_showProfile(ui, p);
+			break;
 		}
 	}
 
-	return 0;
+	if (p != NULL) {
+		people_delete_people(p);
+	}
+	gui_closeUi(ui);
+	free(infos);
 }
